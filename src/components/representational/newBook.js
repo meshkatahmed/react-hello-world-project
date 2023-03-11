cd68bf412f1d667ba27911a46273e12cef26090b
Meshkat Ahammed <meshkatuddinahammed@outlook.com>
2023-03-11 11:22:10 +0600

    Controlled component form

--- src/components/representational/newBook.js
+++ src/components/representational/newBook.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NewBook = props => {
-    return (
-        <div>New book entry</div>
-    );
+class NewBook extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bookName: '',
+            writer: '',
+            description: ''
+        }
+        this.handleInputChange = this.handleInputChange(this);
+        this.handleSubmit = this.handleSubmit(this);
+    }
+    handleSubmit = event => {
+        console.log(this.state);
+        event.preventDefault();
+    }
+    handleInputChange = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>New book entry</h1>
+                <form onSubmit={event => this.handleSubmit(event)}>
+                    <label>Book Name: </label>
+                    <input type='text' name='bookName' value={this.state.bookName}
+                        onChange={this.handleInputChange} />
+                    <br />
+                    <br />
+                    <label>Writer: </label>
+                    <input type='text' name='writer' value={this.state.writer}
+                        onChange={this.handleInputChange} />
+                    <br />
+                    <br />
+                    <label>Description: </label>
+                    <textarea name='description' value={this.state.description}
+                        onChange={this.handleInputChange} />
+                    <br />
+                    <br />
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+        );
+    }
+
 }
 
 export default NewBook;
